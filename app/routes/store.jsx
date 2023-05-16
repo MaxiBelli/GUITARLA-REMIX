902f38bffd1e4d0f2c866c1275a8d8b8ced9aebc
MaxiBelli <maximiliano_belli@hotmail.com>
2023-05-16 17:22:36 -0300

    feat: Consuming Strapi Backend with Loader

--- app/routes/store.jsx
+++ app/routes/store.jsx
@@ -1,5 +1,14 @@
 
 
+export async function loader(){
+    const response = await fetch (`${process.env.API.URL}/guitars?populate=image`)
+    const result = await response.json()
+
+    console.log(result)
+    console.log(process.env.API.URL)
+    return {}
+}
+
 function Store() {
   return (
     <div>store</div>
