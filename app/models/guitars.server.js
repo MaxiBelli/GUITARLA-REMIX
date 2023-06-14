7378735c2287ae286accda5496c4effd908bd4f2
MaxiBelli <maximiliano_belli@hotmail.com>
2023-06-14 15:37:38 -0300

    feat: Dynamic Routing, URL-based Guitar Filtering, and Info Display

--- app/models/guitars.server.js
+++ app/models/guitars.server.js
@@ -3,21 +3,7 @@ export async function getGuitars() {
     return await response.json();
 }
 
-// export async function getGuitars() {
-//     try {
-//       const response = await fetch(`${process.env.API_URL}/guitars?populate=image`);
-//       console.log("API response:", response);
-      
-//       if (!response.ok) {
-//         throw new Error("Error fetching guitars");
-//       }
-      
-//       const data = await response.json();
-//       console.log("Guitars data:", data);
-      
-//       return data;
-//     } catch (error) {
-//       console.error("Error in getGuitars():", error);
-//       throw error;
-//     }
-//   }
+export async function getGuitar(guitarUrl) {
+    const response = await fetch(`${process.env.API_URL}/guitars?filters[url]=${guitarUrl}&populate=image`);
+    return await response.json();
+}
